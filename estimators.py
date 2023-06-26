d65a84a34f6818845603121a17569d52c941f6dd
Douw Marx <douwmarx@gmail.com>
2023-06-26 21:02:33 +0200

    Local linear working

---
+++ estimators.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from music import music_algorithm
+from joblib import Parallel, delayed
+
+# Assumption is that wave numbers or frequency is not a function of space
+
+class LocalLinearSpeedEstimator(object):
+    def __init__(self, wave_data,fs=2000):
+        # Wave data has shape (n_time_steps, n_locations)
+
+        # Remove the mean
+        self.music_spectrum = None
+        self.wave_data = wave_data - np.mean(wave_data, axis=0)
+        self.n_time_steps = wave_data.shape[0]
+        self.n_locations = wave_data.shape[1]
+
+        self.fs = fs
+
+    def get_rps_estimate(self):
+        thetas, music_spectrum = music_algorithm(self.wave_data, 1)
+        theta_max = thetas[np.argmax(music_spectrum)]
+        self.music_spectrum = music_spectrum
+
+        rps = theta_max * self.fs / (2 * np.pi)
+        return rps
+
+class TimeVaryingSpeedEstimator():
+    def __init__(self, wave_data, window_length = 254, overlap = 0.5):
+        self.rps_estimates = None
+        self.wave_data = wave_data
+        self.window_length = window_length
+        self.overlap = overlap
+
+        # Make a list of the indices of the start of each window
+        self.window_start_indices = np.arange(0, self.wave_data.shape[0] - self.window_length, int(self.window_length * (1 - self.overlap)))
+
+    def get_time_varying_rps_estimate(self):
+        # Compute the speed estimate for each window
+        def process(wave_data):
+            estimator = LocalLinearSpeedEstimator(wave_data)
+            return estimator.get_rps_estimate()
+
+        rps_estimates = Parallel(n_jobs=-1)(delayed(process)(self.wave_data[window_start_index:window_start_index + self.window_length, :]) for window_start_index in self.window_start_indices)
+
+        self.rps_estimates = rps_estimates
+        return rps_estimates
+
+    def show_wave_velocity_estimate(self):
+        plt.figure()
+        plt.plot(self.window_start_indices, self.rps_estimates)
+        plt.xlabel("Time")
+        plt.ylabel("rps")
+        # plt.show()
+
+
+
+
--- speed_estimation_on_reduced_signal.py
+++ speed_estimation_on_reduced_signal.py
@@ -1,16 +1,16 @@
 import numpy as np
 import plotly.graph_objects as go
 from music import music_algorithm
-
+from estimators import LocalLinearSpeedEstimator
 
 # Load the .npy dict with the data
 
 
 signals = np.load("10000_frames_pca.npy" , allow_pickle=True).item()
 
-# wave_data = signals["cage"]
+wave_data = signals["cage"]
 # wave_data = signals["outer"]
-wave_data = signals["inner"]
+# wave_data = signals["inner"]
 # Remove the mean
 wave_data = wave_data - np.mean(wave_data, axis=0)
 
@@ -20,12 +20,15 @@ fs = 2000
 spectra = []
 rps = []
 for i in range(0, wave_data.shape[0], window_size):
-    window_wave_data = wave_data[i:i+window_size]
-    thetas, music_spectrum = music_algorithm(window_wave_data, 1)
-    spectra.append(music_spectrum)
-    theta_max = thetas[np.argmax(music_spectrum)]
+    # window_wave_data = wave_data[i:i+window_size]
+    # thetas, music_spectrum = music_algorithm(window_wave_data, 1)
+    # spectra.append(music_spectrum)
+    # theta_max = thetas[np.argmax(music_spectrum)]
+    # rps.append(theta_max *fs / (2*np.pi))
 
-    rps.append(theta_max *fs / (2*np.pi))
+    estimator = LocalLinearSpeedEstimator(wave_data[i:i+window_size, :])
+    rps.append(estimator.get_rps_estimate())
+    spectra.append(estimator.music_spectrum)
 
 # Plot a heatmap of how the spectrum changes over time
 
